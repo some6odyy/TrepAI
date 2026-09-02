532438d4b46e769cd59280833895759f7386751e
some6odyy <yohelignacio21@gmail.com>
2026-09-02 16:27:36 -0400

    Enhance database engine configuration options

--- app/db/database.py
+++ app/db/database.py
@@ -10,7 +10,12 @@ from app.core.config import DATABASE_URL
 
 connect_args = {"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
 
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args=connect_args,
+    pool_pre_ping=True,
+    pool_recycle=300,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
